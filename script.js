2e58fb479b0f2988363b647dfb61fe41a4096bba
tfalcongreen <66723909+tfalcongreen@users.noreply.github.com>
2022-05-03 09:31:25 +1000

    added finder function

--- script.js
+++ script.js
@@ -23,13 +23,18 @@ function fetchData() {
 }
 
 fetchData();
-
+pollSite();
 function pollSite() {
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tabs[0].url;
-        // use `url` here inside the callback because it's asynchronous!
-    
-    fetch(url)
+
+    //TODO - Implement "Get" url from active page - needs to be after user action
+    // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    //     let url = tabs[0].url;
+    //     // use `url` here inside the callback because it's asynchronous!
+    // });
+
+    let url = 'https://www.theiconic.com.au/unisex-1460-nappa-8-eye-boots-93225.html'
+
+    let result = fetch(url)
     .then(function(response) {
         // When the page is loaded convert it to text
         return response.text()
@@ -38,31 +43,57 @@ function pollSite() {
         // Initialize the DOM parser
         let parser = new DOMParser();
 
-      
-
         // Parse the text
         let doc = parser.parseFromString(html, "text/html");
-        // var getPrice = doc.getElementsByClassName("price final")[0];
-        // var oldPrice = doc.getElementsByClassName("price final")[0].textContent;
 
-        let selectedSize = doc.getElementsByClassName("small-7 medium-6 columns the-size");
-        let ss = doc.querySelector('[class="dropdown-label"]').textContent;
-        console.log(ss)
-        let sku = doc.querySelector('[data-key="load"]').textContent;
+        //let selectedSize = doc.getElementsByClassName("small-7 medium-6 columns the-size");
 
-        console.log(sk);
-        fetch('https://eve.theiconic.com.au/v2/catalog/products/'+sku)
+        //get user selected size from dom dropdown
+        let selected_size = doc.querySelector('[class="dropdown-label"]').textContent;
+
+        console.log(selected_size);
+
+        //get Product ID from Dom
+        let pid = doc.querySelector('[data-key="load"]').textContent;
+
+        //remove "[]" and format pid
+        let split = pid.replace(/[^a-zA-Z0-9]/g, "")
+
+        console.log(split);
+
+        //fetch all data via api
+        return fetch('https://eve.theiconic.com.au/v2/catalog/products/'+split); 
+        })
+        //parse response as json
+        .then(response => response.json())
+        // catch request error
+        .catch(err => {
+            console.error('Request failed', err) 
+        })
+        //pass json from second url
+        result.then(function(r){
+            return r
+        }); 
+        console.log(result)
+    var found = findObject(result);
+
+    console.log(found)
+
+
+
+
+
 
         //console.log(selectedSize[0]);
         //var newS = selectedSize.getElementsByClassName('original-size').textContent;
         //console.log(newS);
 
-        let choice = " 4 "
+        // let choice = " 4 "
 
-        for (var i=0; i < selectedSize.length; i++) {
-            console.log(selectedSize[i].getElementsByClassName('original-size')[i].textContent);
-            console.log(selectedSize.length);
-            console.log("done");
+        // for (var i=0; i < selectedSize.length; i++) {
+        //     console.log(selectedSize[i].getElementsByClassName('original-size')[i].textContent);
+        //     console.log(selectedSize.length);
+        //     console.log("done");
             // if(selectedSize[i].getElementsByClassName('original-size')[i].textContent == choice){
             //    var result = selectedSize[i].getElementsByClassName('original-size')[i].textContent;
             //     // result=selected[i].textContent;
@@ -72,34 +103,55 @@ function pollSite() {
             // }
                 
             
-        }
+        // }
         // console.log("thisisthenewsize "+result);
         
 
 
-        
-    });
+    // priceAlert();
 
-        console.log(oldPrice)
+    // });
 
-        let td = document.createElement('td');
-        td.textContent = newPrice;
+function findObject(result) {
 
-        document.getElementById("newprice").appendChild(doc.getElementsByClassName("price final")[0]);
-
-        var newPrice = document.getElementById("lastprice").textContent;
-     
-        var number1 = Number(oldPrice.replace(/[^0-9.-]+/g,""));
-        var number2 = Number(newPrice.replace(/[^0-9.-]+/g,""));
-
-        console.log(number1)
-        console.log(number2)
-
-        if (number2 = number1){
-
-            //alert("PRICE CHANGE ALERT")
+    var find = []
+    console.log(result); // http Request result as JSON
+    for (var i=0;i < result._embedded.simples.length; i++ ){
+        if (result._embedded.simples[i].size_code == selected_size){
+            console.log(result._embedded.simples[i].size_code)
+            find.push(result._embedded.simples[i].size_code)
+        }else{
+            console.log("Size not found select another")
         }
-})
-}   
+    }
+    console.log(find);
 
-setInterval(pollSite, 10000);
+    return find
+
+
+}
+
+// function priceAlert()
+//         console.log(oldPrice)
+
+//         let td = document.createElement('td');
+//         td.textContent = newPrice;
+
+//         document.getElementById("newprice").appendChild(doc.getElementsByClassName("price final")[0]);
+
+//         var newPrice = document.getElementById("lastprice").textContent;
+     
+//         var number1 = Number(oldPrice.replace(/[^0-9.-]+/g,""));
+//         var number2 = Number(newPrice.replace(/[^0-9.-]+/g,""));
+
+//         console.log(number1)
+//         console.log(number2)
+
+//         if (number2 = number1){
+
+//             //alert("PRICE CHANGE ALERT")
+//         }
+}
+
+
+// setInterval(pollSite, 10000);
